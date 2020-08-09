6b84839e1a4eaf3d2b062173876bfe79053ff55e
Boye A <boyeadeotan@gmail.com>
2020-08-09 16:50:55 +0100

    additional commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Get range value
 
 const getTip =()=>{
+    let billAmount = document.getElementById("bill").value;
     let tipValue = document.getElementById("slider").value;
     tipValue = parseInt(tipValue);  
     if (tipValue != 0){
